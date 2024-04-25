8f4ce81c151dd51def11006db0b385e3de6eff5f
SkyMXF <skymxf@gmail.com>
2024-04-25 11:43:27 +0800

    use flask to start server

--- run_bot.py
+++ run_bot.py
@@ -1,61 +1,42 @@
 import json
-import xml.etree.ElementTree
-import http.server
-import socketserver
 import logging
+from flask import Flask, request
 from wechatpy.utils import check_signature
 from wechatpy.exceptions import InvalidSignatureException
 from wechatpy import parse_message, create_reply
 
+app = Flask(__name__)
 
 LOGGER = logging.getLogger("MainLogger")
 
 TOKEN = ""
 
 
-class WeChatUserMsgRequestHandler(http.server.SimpleHTTPRequestHandler):
+@app.route('/memebot', methods=['GET', 'POST'])
+def meme_bot():
+    signature = request.args.get('signature', '')
+    timestamp = request.args.get('timestamp', '')
+    nonce = request.args.get('nonce', '')
+    echo_str = request.args.get('echostr', '')
 
-    def do_GET(self):
-        # get args
-        args = self.path.split("?")[1]
-        args = args.split("&")
-        args_dict = {}
-        for arg in args:
-            key, value = arg.split("=")
-            args_dict[key] = value
+    # check signature
+    try:
+        check_signature(TOKEN, signature, timestamp, nonce)
+    except InvalidSignatureException:
+        return 'Verification failed'
 
-        try:
-            check_signature(TOKEN, args_dict["signature"], args_dict["timestamp"], args_dict["nonce"])
-        except InvalidSignatureException:
-            LOGGER.error("Invalid signature for verification.")
-            return
-
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(args_dict["echostr"].encode())
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        LOGGER.debug("Got post data: " + str(post_data))
-        self.send_response(200)
-        self.end_headers()
-
-        # parse xml data
-        root = xml.etree.ElementTree.fromstring(post_data)
-        to_user_name = root.find("ToUserName").text
-        from_user_name = root.find("FromUserName").text
-        create_time = root.find("CreateTime").text
-        msg_type = root.find("MsgType").text
-        media_id = root.find("MediaId").text
-
-        LOGGER.info("To user name: " + to_user_name)
-        LOGGER.info("From user name: " + from_user_name)
-        LOGGER.info("Create time: " + create_time)
-        LOGGER.info("Msg type: " + msg_type)
-        LOGGER.info("Media id: " + media_id)
-
-        # response
+    if request.method == 'GET':
+        # verification reply
+        return echo_str
+    else:
+        # handle message
+        msg = parse_message(request.data)
+        if msg.type == 'text':
+            reply = create_reply('echo: ' + msg.content, msg)
+            return reply.render()
+        else:
+            reply = create_reply('Sorry, can not handle this for now', msg)
+            return reply.render()
 
 
 if __name__ == '__main__':
@@ -75,6 +56,5 @@ if __name__ == '__main__':
     port = config["local_port"]
     TOKEN = config["token"]
 
-    with socketserver.TCPServer(("", port), WeChatUserMsgRequestHandler) as httpd:
-        LOGGER.info("Server running on port: " + str(port))
-        httpd.serve_forever()
+    LOGGER.info("Server running on port: " + str(port))
+    app.run(port=port)
